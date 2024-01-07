76673a9c5a194f64c3b11d0158512d6f0c4da2cb
Chi Hung Wong <info@spotlight-platform.com>
2024-01-07 23:29:16 +0800

    update: Update the description in setup.py file

--- python/setup.py
+++ python/setup.py
@@ -4,7 +4,7 @@ setup(
     name='x3dh',
     packages=find_packages(include=['x3dh']),
     version='0.0.1',
-    description='My first Python library',
+    description='Embeded the xxxdh, which iplementation of the Extended Triple Diffie-Hellman key exchange protocol written in Rust, on Python. xxxdh is written by Olexander Yermakov.',
     author='Klaus Wong',
     install_requires=[],
 )
